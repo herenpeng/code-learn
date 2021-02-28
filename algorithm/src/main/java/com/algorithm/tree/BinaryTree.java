fac10d84e02e523c2a62e96dfe1d21ac7680e198
herenpeng <2519832582@qq.com>
2021-02-28 17:55:53 +0800

    数据结构之什么是树.md

--- algorithm/src/main/java/com/algorithm/tree/BinaryTree.java
+++ algorithm/src/main/java/com/algorithm/tree/BinaryTree.java
@@ -5,4 +5,113 @@ package com.algorithm.tree;
  * @since 2021-02-27 11:01
  */
 public class BinaryTree {
+
+    /**
+     * 二叉树根节点指针
+     */
+    private TreeNode root;
+
+    /**
+     * 获取二叉树的根节点
+     *
+     * @return 二叉树的根节点
+     */
+    public TreeNode root() {
+        return this.root;
+    }
+
+    /**
+     * 通过数组创建二叉树时数组的索引位置
+     */
+    private int createBinaryTreeByArrIndex = 0;
+
+    /**
+     * 通过数组创建二叉树的构造方法
+     *
+     * @param arr 数组
+     */
+    public BinaryTree(Integer[] arr) {
+        // 通过数组构建二叉树，并保存二叉树根节点的指针
+        this.root = createBinaryTree(arr);
+    }
+
+    /**
+     * 创建二叉树的方法
+     *
+     * @param arr 数组
+     * @return root根节点指针
+     */
+    public TreeNode createBinaryTree(Integer[] arr) {
+        TreeNode node = null;
+        // 如果数组为空，或者索引越界，则直接返回空的二叉树
+        if (arr == null || arr.length == 0 || createBinaryTreeByArrIndex < 0
+                || createBinaryTreeByArrIndex > arr.length - 1) {
+            return null;
+        }
+        // 通过索引获取数组对应位置的数据
+        Integer data = arr[createBinaryTreeByArrIndex++];
+        if (data != null) {
+            // 如果这个数据不为空，则创建节点，并返回
+            node = new TreeNode(data);
+            // 数组的下一个节点为该节点的左孩子节点
+            node.leftChild = createBinaryTree(arr);
+            // 数组的下一个节点为该节点的右孩子节点
+            node.rightChile = createBinaryTree(arr);
+        }
+        return node;
+    }
+
+    /**
+     * 二叉树节点
+     */
+    class TreeNode {
+        /**
+         * 二叉树节点存储节点数据的变量
+         */
+        private Integer data;
+        /**
+         * 二叉树节点左孩子节点指针
+         */
+        private TreeNode leftChild;
+        /**
+         * 二叉树节点右孩子节点指针
+         */
+        private TreeNode rightChile;
+
+        /**
+         * 获取节点数据的方法
+         *
+         * @return 节点数据
+         */
+        public Integer data() {
+            return this.data;
+        }
+
+        /**
+         * 获取节点左孩子节点指针的方法
+         *
+         * @return 节点左孩子节点指针
+         */
+        public TreeNode leftChild() {
+            return this.leftChild;
+        }
+
+        /**
+         * 获取节点右孩子节点指针的方法
+         *
+         * @return 节点右孩子节点指针
+         */
+        public TreeNode rightChile() {
+            return this.rightChile;
+        }
+
+        /**
+         * 节点构造方法
+         *
+         * @param data 节点数据
+         */
+        TreeNode(Integer data) {
+            this.data = data;
+        }
+    }
 }
---
+++ algorithm/src/main/java/com/algorithm/tree/BinaryTreeTest.java
@@ -0,0 +1,195 @@
+package com.algorithm.tree;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author herenpeng
+ * @since 2021-02-28 12:26
+ */
+public class BinaryTreeTest {
+
+    @Test
+    public void test01() {
+        Integer[] arr = new Integer[]{3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        // 构建二叉树
+        BinaryTree binaryTree = new BinaryTree(arr);
+        // 获取二叉树的根节点
+        BinaryTree.TreeNode root = binaryTree.root();
+        System.out.println("前序遍历：");
+        // 前序遍历
+        preOrderTraveral(root);
+        System.out.println();
+        preOrderTraveralWithStack(root);
+    }
+
+    /**
+     * 前序遍历
+     *
+     * @param node 节点
+     */
+    public void preOrderTraveral(BinaryTree.TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data() + "\t");
+        preOrderTraveral(node.leftChild());
+        preOrderTraveral(node.rightChile());
+    }
+
+    /**
+     * 前序遍历，非递归
+     *
+     * @param root 二叉树根节点
+     */
+    public void preOrderTraveralWithStack(BinaryTree.TreeNode root) {
+        Stack<BinaryTree.TreeNode> stack = new Stack<>();
+        BinaryTree.TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                System.out.print(treeNode.data() + "\t");
+                stack.push(treeNode);
+                treeNode = treeNode.leftChild();
+            }
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                treeNode = treeNode.rightChile();
+            }
+        }
+    }
+
+
+    @Test
+    public void test02() {
+        Integer[] arr = new Integer[]{3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        // 构建二叉树
+        BinaryTree binaryTree = new BinaryTree(arr);
+        // 获取二叉树的根节点
+        BinaryTree.TreeNode root = binaryTree.root();
+        System.out.println("中序遍历：");
+        // 中序遍历
+        inOrderTraveral(root);
+        System.out.println();
+        inOrderTraveralWithStack(root);
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param node 节点
+     */
+    public void inOrderTraveral(BinaryTree.TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraveral(node.leftChild());
+        System.out.print(node.data() + "\t");
+        inOrderTraveral(node.rightChile());
+    }
+
+    /**
+     * 中序遍历，非递归
+     *
+     * @param root 二叉树根节点
+     */
+    public void inOrderTraveralWithStack(BinaryTree.TreeNode root) {
+        Stack<BinaryTree.TreeNode> stack = new Stack<>();
+        BinaryTree.TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                stack.push(treeNode);
+                treeNode = treeNode.leftChild();
+            }
+            if (!stack.isEmpty()) {
+                treeNode = stack.pop();
+                System.out.print(treeNode.data() + "\t");
+                treeNode = treeNode.rightChile();
+            }
+        }
+    }
+
+
+    @Test
+    public void test03() {
+        Integer[] arr = new Integer[]{3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        // 构建二叉树
+        BinaryTree binaryTree = new BinaryTree(arr);
+        // 获取二叉树的根节点
+        BinaryTree.TreeNode root = binaryTree.root();
+        System.out.println("后序遍历：");
+        // 后序遍历
+        postOrderTraveral(root);
+        System.out.println();
+        postOrderTraveralWithStack(root);
+    }
+
+
+    /**
+     * 后序遍历
+     *
+     * @param node 节点
+     */
+    public void postOrderTraveral(BinaryTree.TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrderTraveral(node.leftChild());
+        postOrderTraveral(node.rightChile());
+        System.out.print(node.data() + "\t");
+    }
+
+    /**
+     * 后序遍历，非递归
+     *
+     * @param root 二叉树根节点
+     */
+    public void postOrderTraveralWithStack(BinaryTree.TreeNode root) {
+
+    }
+
+
+    @Test
+    public void test04() {
+        Integer[] arr = new Integer[]{3, 2, 9, null, null, 10, null, null, 8, null, 4};
+        // 构建二叉树
+        BinaryTree binaryTree = new BinaryTree(arr);
+        // 获取二叉树的根节点
+        BinaryTree.TreeNode root = binaryTree.root();
+        System.out.println("层序遍历：");
+        // 层序遍历
+        levelOrderTraveralWithQueue(root);
+    }
+
+    /**
+     * 层序遍历
+     *
+     * @param node 二叉树节点
+     */
+    public void levelOrderTraveral(BinaryTree.TreeNode node) {
+    }
+
+    /**
+     * 层序遍历
+     *
+     * @param root 二叉树根节点
+     */
+    public void levelOrderTraveralWithQueue(BinaryTree.TreeNode root) {
+        Queue<BinaryTree.TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            BinaryTree.TreeNode node = queue.poll();
+            System.out.print(node.data() + "\t");
+            BinaryTree.TreeNode leftChild = node.leftChild();
+            if (leftChild != null) {
+                queue.offer(leftChild);
+            }
+            BinaryTree.TreeNode rightChile = node.rightChile();
+            if (rightChile != null) {
+                queue.offer(rightChile);
+            }
+        }
+    }
+
+
+}
